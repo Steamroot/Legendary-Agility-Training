4347186c4cfc906412da9df3c6d0032a48a0bc85
Steamroot <samuelalejandro08x@gmail.com>
2020-06-29 21:20:23 -0400

    Freeze body when popup opens

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+//--------------------------------- Variables ---------------------------------------------------------------------------------------
+const bodyElement = document.getElementById('page');
+
+const openPopup = document.getElementsByName('popup-open');
+
+const popupButton = document.getElementsByName('popup-button');
+
+const closePopup = document.getElementsByClassName('popup__close');
+//---------------------------------- Close info banner ------------------------------------------------------------------------------
 document.getElementById('close-info-banner').addEventListener('click', () => {
     document.querySelector('.header__info-box').style.display = 'none';
 });
+//---------------------------------- Open the popup overlay --------------------------------------------------------------------------
+for (let i = 0; i < popupButton.length; i++) {
+    popupButton[i].addEventListener('click', () => {
+        openPopup[i].style.cssText = 'visibility: visible; opacity: 1;';
+        bodyElement.style.overflow = 'hidden';
+    });
+}
+//---------------------------------- Close the popup overlay --------------------------------------------------------------------------
+for (let i = 0; i < closePopup.length; i++) {
+    closePopup[i].addEventListener('click', () => {
+        openPopup[i].style.cssText = 'visibility: hidden; opacity: 0;';
+        bodyElement.style.overflow = 'auto'
+    });
+}
